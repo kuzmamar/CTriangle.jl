d533414dcf3feb9460d620ce323b852f41fd0104
kuzmamar <martin.kuzma90@gmail.com>
2016-05-15 23:29:19 +0200

    testing ompatibility

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,9 @@
-using Base.Test, CTriangle
+if VERSION >= v"0.5.0-dev+7720"
+    using Base.Test
+else
+    using BaseTestNext
+    const Test = BaseTestNext
+end
+using CTriangle
 
 include("Switches.jl")
