ff4bc00e3298f744401e03e3b3f4022310e83e60
Martin Kuzma <martin.kuzma90@gmail.com>
2016-10-23 21:51:49 +0200

    deleting unused files

--- test/Triangulations.jl
+++
@@ -1,7 +0,0 @@
-println("Triangulation tests")
-s = CTriangle.NodesSwitches()
-CTriangle.setneighbor!(s)
-CTriangle.setedge!(s)
-CTriangle.setnomarker!(s)
-t = CTriangle.triangulate("test_files/nodes1", s)
-@test isa(t, CTriangle.Triangulation) == true
